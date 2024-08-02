8bd0f9457f7c2ea9f192864fc60496489b6cfb78
WarrenQuan <quanwarren11@gmail.com>
2024-08-02 11:37:02 -0400

    changing href

--- src/app/page.js
+++ src/app/page.js
@@ -170,7 +170,6 @@ export default function Home() {
             <Button
               onClick={() => signIn("google")}
               variant="contained"
-              href="#contained-buttons"
               color="primary"
               style={{
                 backgroundColor: "black",
